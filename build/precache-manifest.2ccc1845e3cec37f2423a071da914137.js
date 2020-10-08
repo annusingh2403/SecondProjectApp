a00cee683ff0e152bf824db0cdba02882164d1d5
annu_singh <annusingh2403@gmail.com>
2020-10-09 00:13:57 +0530

    first commit

---
+++ build/precache-manifest.2ccc1845e3cec37f2423a071da914137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9118584f6ebc873015ca4fb2e1185ae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53c110b17f804053385f",
+    "url": "/static/css/main.b7d27ffb.chunk.css"
+  },
+  {
+    "revision": "645aeb950fe93f4b1bda",
+    "url": "/static/js/2.f4798460.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f4798460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c110b17f804053385f",
+    "url": "/static/js/main.adb6d5c1.chunk.js"
+  },
+  {
+    "revision": "d1083c3165cfdb935f61",
+    "url": "/static/js/runtime-main.8297bba0.js"
+  }
+]);
